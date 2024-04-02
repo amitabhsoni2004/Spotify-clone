7319c8f1135f680133e520f79594469ec660116e
Amitabh Soni <144244954+amitabhsoni2004@users.noreply.github.com>
2024-04-02 08:04:42 +0530

    up2

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ return `${formattedMinutes}:${formattedSeconds}`;
 
 async function getSongs(folder){
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML=response;
@@ -70,7 +70,7 @@ const playMusic = (track,pause=false)=>{
 }    
 
 async function displayAlbums(){
-    let a = await fetch(`http://127.0.0.1:3000/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML=response;
@@ -85,7 +85,7 @@ async function displayAlbums(){
         //    console.log(folder);
            
             //get the meta data of the folder
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+            let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json();
             // console.log(response);
             cardContainer.innerHTML = cardContainer.innerHTML  + `<div data-folder="${folder}" class="card">
